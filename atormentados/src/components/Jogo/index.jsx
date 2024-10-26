5c6b23f58f78ed693fe510c7d6c1733b9ff41f4f
Laboratorio <laboratorio@uniprojecao.edu.br>
2024-10-26 04:13:47 -0300

    mais acessibilidade

--- atormentados/src/components/Jogo/index.jsx
+++ atormentados/src/components/Jogo/index.jsx
@@ -7,21 +7,12 @@ const Jogo = () => {
     return (
         <>
             <div className="jogo-container">
-            <div className="sound-icon">
-                    icone do som 
-                </div>
+        
                 <div className="jogo">
                     <div className="img-jogo">
                         <img src={imgteste} alt="/*aqui vem o nome da imagem do banco*/" />
                     </div>
                     <div className="jogo-dentro">
-                        <div className="letras-jogo">
-                            <Letra />
-                            <Letra />
-                            <Letra />
-                            <Letra />
-                            <Letra />
-                        </div>
                         <div className="letras-jogo">
                             <Letra />
                             <Letra />
